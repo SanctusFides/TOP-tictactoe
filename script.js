8b52dd0ad240aff18001d9f0667780b2953c0a30
John <jhn.hines@gmail.com>
2023-06-16 17:40:45 -0500

    Announcement and half of wincheck logic added

--- script.js
+++ script.js
@@ -23,34 +23,72 @@ const gameBoard = (() => {
     // Creating the players below and assigning their marker
     const human = player('X');
     const npc = player('O');
+    const annoucement = document.getElementById('annoucement');
+
 
     // variable is used to keep track of which turn it is - flips in the makePlay funct
     let currentTurn = human;
 
     const makePlay = (posID) => {
+        // This line here clears any possible text about a position being occupied
+        annoucement.innerHTML = " ";
         if (board[posID] === ' ') {
-
+            
             const boardSpot = document.getElementById(posID);
             boardSpot.innerHTML = currentTurn.playerMarker;
             board[posID] = currentTurn.playerMarker;
             
             if (currentTurn === human) {
+                wincheck();
                 currentTurn = npc;
             } else {
+                wincheck();
                 currentTurn = human;
             }
         } else {
             // forrest gump easter 
+            annoucement.innerHTML = "That spot is already taken";
             console.log('seats taken');
         }
     }
+
+    const wincheck = () => {
+        // Loops through each row and both diagonals to check if they all match the player marker
+
+        if (board[0] === human.playerMarker && board[1] === human.playerMarker && board[2] === human.playerMarker 
+        || board[3] === human.playerMarker && board[4] === human.playerMarker && board[5] === human.playerMarker
+        || board[6] === human.playerMarker && board[7] === human.playerMarker && board[8] === human.playerMarker
+
+        || board[0] === human.playerMarker && board[3] === human.playerMarker && board[6] === human.playerMarker
+        || board[1] === human.playerMarker && board[4] === human.playerMarker && board[7] === human.playerMarker
+        || board[2] === human.playerMarker && board[5] === human.playerMarker && board[8] === human.playerMarker
+
+        || board[0] === human.playerMarker && board[4] === human.playerMarker && board[8] === human.playerMarker
+        || board[2] === human.playerMarker&& board[4] === human.playerMarker && board[6] === human.playerMarker) {
+            console.log('YOU WIN!');
+            annoucement.innerHTML = 'You win!';
+        }
+    }
+
+    // Resets the array and clears the board and announcement section
+    const resetBoard = () => {
+        board = [" "," "," "," "," "," "," "," "," "];
+        for (let button of buttons) {
+            button.innerHTML = " ";
+        };
+        annoucement.innerHTML = " ";
+    }
+    
+
 
     const testPrint = () => {
         console.log(board);
     }
 
     return {
-        makePlay
+        makePlay,
+        resetBoard,
+        testPrint
     }
 })();
 
