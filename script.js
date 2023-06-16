9583ed82b176e8321cbb44e7c77a27e3ce12c8d9
John <jhn.hines@gmail.com>
2023-06-16 16:11:45 -0500

    Marker logic for the board is completed

--- script.js
+++ script.js
@@ -2,31 +2,46 @@
 
 //collect all the buttons, add a listener to each and then have a function to receive a click
 const buttons = document.getElementsByClassName("pos-button");
-
 const buttonClicked = e => {
     const idString = e.target.id;
-    // console.log(idString);
-    gameBoard.makePlay(idString)
+    gameBoard.makePlay(idString);
 }
-
 for (let button of buttons) {
     button.addEventListener("click", buttonClicked);
 }
 
 
 // Gamelogic goes below here
+const player = (marker) => {
+    const playerMarker = marker;
 
-
+    return {playerMarker};
+}
 const gameBoard = (() => {
     let board = [" "," "," "," "," "," "," "," "," "];
 
-    const makePlay = (a) => {
-        if (board[a] === ' ') {
-            board[a] = a;
-            testPrint();
+    // Creating the players below and assigning their marker
+    const human = player('X');
+    const npc = player('O');
+
+    // variable is used to keep track of which turn it is - flips in the makePlay funct
+    let currentTurn = human;
+
+    const makePlay = (posID) => {
+        if (board[posID] === ' ') {
+
+            const boardSpot = document.getElementById(posID);
+            boardSpot.innerHTML = currentTurn.playerMarker;
+            board[posID] = currentTurn.playerMarker;
+            
+            if (currentTurn === human) {
+                currentTurn = npc;
+            } else {
+                currentTurn = human;
+            }
         } else {
-            // forrest gump easter egg
-            console.log('this seats taken');
+            // forrest gump easter 
+            console.log('seats taken');
         }
     }
 
@@ -39,8 +54,3 @@ const gameBoard = (() => {
     }
 })();
 
-
-const player = () => {
-
-
-}
