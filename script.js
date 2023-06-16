3a7e8bc4431227de67cdb7275b49cdf5b20d5edd
John <jhn.hines@gmail.com>
2023-06-16 13:11:27 -0500

    Board backend working - next step is the visual

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
+// Top portion is for page/button logic
+
+//collect all the buttons, add a listener to each and then have a function to receive a click
 const buttons = document.getElementsByClassName("pos-button");
 
-
-const testFunct = e => {
+const buttonClicked = e => {
     const idString = e.target.id;
-    const idSplit = idString.split(' ');
-    const row = idSplit[0];
-    const rowValue = row.slice(-1);
-
-    const pos = idSplit[1];
-    const posValue = pos.slice(-1);
-    console.log(rowValue + posValue);
+    // console.log(idString);
+    gameBoard.makePlay(idString)
 }
-
 
 for (let button of buttons) {
-    button.addEventListener("click", testFunct);
+    button.addEventListener("click", buttonClicked);
 }
 
-let board = [];
 
-const gameBoard = ((board) => {
-
+// Gamelogic goes below here
 
 
+const gameBoard = (() => {
+    let board = [" "," "," "," "," "," "," "," "," "];
+
+    const makePlay = (a) => {
+        if (board[a] === ' ') {
+            board[a] = a;
+            testPrint();
+        } else {
+            // forrest gump easter egg
+            console.log('this seats taken');
+        }
+    }
+
+    const testPrint = () => {
+        console.log(board);
+    }
+
+    return {
+        makePlay
+    }
 })();
 
 
